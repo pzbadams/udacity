2e4d824d5da3c83c3d969a002d81b43ba1d470aa
pzbadams <pzbadams@users.noreply.github.com>
2015-07-16 17:58:28 -0700

    Update tournament.py

--- tournament.py
+++ tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -17,7 +17,8 @@ def deleteMatches():
     c = db.cursor()
     c.execute("DELETE FROM Matches")
     db.commit()
-    db.close();
+    db.close()
+
 
 def deletePlayers():
     """Remove all the player records from the database."""
@@ -25,7 +26,8 @@ def deletePlayers():
     c = db.cursor()
     c.execute("DELETE FROM Players")
     db.commit()
-    db.close();
+    db.close()
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
@@ -36,26 +38,28 @@ def countPlayers():
     db.close()
     return rows[0][0]
 
+
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
     db = connect()
     c = db.cursor()
-    c.execute("INSERT INTO Players (name) VALUES (%s)",(name,))
+    c.execute("INSERT INTO Players (name) VALUES (%s)", (name,))
     db.commit()
-    db.close();
+    db.close()
+
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place,
+    or a player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -71,6 +75,7 @@ def playerStandings():
     db.close()
     return rows
 
+
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
 
@@ -81,22 +86,23 @@ def reportMatch(winner, loser):
     db = connect()
     c = db.cursor()
     if draw:
-        c.execute("INSERT INTO Matches (player,opponent,result) VALUES (%s,%s,1)",(winner,loser))
-        c.execute("INSERT INTO Matches (player,opponent,result) VALUES (%s,%s,1)",(loser,winner))
+        c.execute("INSERT INTO Matches (player, opponent, result) VALUES (%s, %s, 1)", (winner, loser))
+        c.execute("INSERT INTO Matches (player, opponent, result) VALUES (%s, %s, 1)", (loser, winner))
     else:
-        c.execute("INSERT INTO Matches (player,opponent,result) VALUES (%s,%s,1)",(winner,loser))
-        c.execute("INSERT INTO Matches (player,opponent,result) VALUES (%s,%s,0)",(loser,winner))
+        c.execute("INSERT INTO Matches (player, opponent, result) VALUES (%s, %s, 1)", (winner, loser))
+        c.execute("INSERT INTO Matches (player, opponent, result) VALUES (%s, %s, 0)", (loser, winner))
     db.commit()
-    db.close();
- 
+    db.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -109,14 +115,13 @@ def swissPairings():
     c.execute("SELECT id,name,wins FROM Standings ORDER BY wins DESC;")
     rows = c.fetchall()
     db.close()
-    i=0
+    i = 0
     pairings = []
     while i < len(rows):
         playerAid = rows[i][0]
         playerAname = rows[i][1]
         playerBid = rows[i+1][0]
         playerBname = rows[i+1][1]
-        pairings.append((playerAid,playerAname,playerBid,playerBname))
-        i=i+2
-
+        pairings.append((playerAid, playerAname, playerBid, playerBname))
+        i = i + 2
     return pairings
